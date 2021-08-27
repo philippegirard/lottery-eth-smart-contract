24aa64ed196efb9e7fca16c3b4a65efdc570339e
Philippe Girard <philippe.girard5@gmail.com>
2021-08-27 14:25:40 -0400

    template contract

---
+++ eth2/deploy.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const {interface, bytecode} = require("./compile");
+
+const provider = new HDWalletProvider(
+    process.env['META_MNEMONIC'],
+    process.env['INFURA_ENDPOINT']
+)
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    // const accounts = await web3.eth.getAccounts();
+    const deployerAddress = process.env["DEPLOYER_ACCOUNT_ADDR"];
+
+    console.log("Attempting to deploy from account", deployerAddress);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode})
+        .send({
+            gas: "1000000",
+            gasPrice: '5000000000',
+            from: deployerAddress
+        });
+
+    console.log("Contract deployed to", result.options.address);
+};
+deploy();
