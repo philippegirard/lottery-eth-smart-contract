786815466e038eb96191da9b940e5c25024c1f36
Philippe Girard <philippe.girard5@gmail.com>
2021-08-27 11:05:24 -0400

    deploy contracts

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,29 @@
-// contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3')
+const {interface, bytecode} = require('../compile')
+
+const web3 = new Web3(ganache.provider());
+
+let accounts;
+let inbox;
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({
+            data: bytecode,
+            arguments: ["Hi there!"]
+        })
+        .send({
+            from: accounts[0],
+            gas: '1000000'
+        })
+
+})
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(accounts)
+    });
+})
