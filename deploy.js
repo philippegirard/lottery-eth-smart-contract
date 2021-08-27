06c8f652fc1b4135e7f30c2975afbb92f97029dc
Philippe Girard <philippe.girard5@gmail.com>
2021-08-27 11:40:44 -0400

    metamask dev

--- deploy.js
+++ deploy.js
@@ -1,1 +1,10 @@
-// deploy code will go here
+require('dotenv').config();
+
+const HDWalletProvider = require('truffle-hdwallet-provider')
+const Web3 = require('web3')
+const { interface, bytecode } = require('./compile')
+
+const provider = new HDWalletProvider(
+    process.env['META_MNEMONIC'],
+    process.env['INFURA_ENDPOINT']
+)
