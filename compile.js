1643015b45f618b71c2e9c6cea7047cc6eb8d52c
Philippe Girard <philippe.girard5@gmail.com>
2021-08-27 10:50:16 -0400

    compile code

--- compile.js
+++ compile.js
@@ -1,1 +1,10 @@
 // compile code will go here
+
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc")
+
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf8")
+
+console.log(solc.compile(source, 1))
